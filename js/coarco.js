4c96e5ae510427553510c38df7500ef550fc080e
dbrouard <diego@net-monsters.com>
2013-02-12 17:09:39 +0000

    offers block maximenu pinterest blocks layout

--- js/coarco.js
+++ js/coarco.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($){
-    jQuery(".highlight_block h2,.view-mode-pinterest.product_block .field-name-field-subtitulo-destacado").each( function(){
+    jQuery(".ofertas_block h2,.highlight_block h2,.pinterest .field-name-field-subtitulo-destacado").each( function(){
         jQuery(this).append("<div class='block_left_wing'></div>");
         jQuery(this).append("<div class='block_right_wing'></div>");
         jQuery(this).append("<div class='block_left_expand'></div>");
         jQuery(this).append("<div class='block_right_expand'></div>");
     });
-    jQuery(".product_block.view-mode-pinterest").each( function(index){
+    /*jQuery(".pinterest").each( function(index){
        jQuery(this).addClass("count"+(index+1));
     });
 });
