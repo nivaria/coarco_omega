ba914e079800ab7a5b81acee9be6a043f67eb91d
dbrouard <diego@net-monsters.com>
2013-02-13 12:56:36 +0000

    bloque destacado + pinterest

--- js/coarco.js
+++ js/coarco.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){
-    jQuery(".ofertas_block h2,.highlight_block h2,.pinterest .field-name-field-subtitulo-destacado").each( function(){
+    jQuery(".ofertas_block h2,.pinterest .field-name-field-subtitulo-destacado").each( function(){
         jQuery(this).append("<div class='block_left_wing'></div>");
         jQuery(this).append("<div class='block_right_wing'></div>");
         jQuery(this).append("<div class='block_left_expand'></div>");
