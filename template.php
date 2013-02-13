83b9842dd0f14aec3b91ad333779fe40a519fd29
dbrouard <diego@net-monsters.com>
2013-02-13 10:48:03 +0000

    breadcrumbs, pager

--- template.php
+++ template.php
@@ -24,3 +24,15 @@ function coarco_omega_preprocess_html(&$vars) {
   
   drupal_add_css("http://fonts.googleapis.com/css?family=Exo","external");
 }
+function coarco_omega_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode('', $breadcrumb) . '</div>';
+    return $output;
+  }
+}
