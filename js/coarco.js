7cd3f085bfcce840877166ad31b226ff451f9045
dbrouard <diego@net-monsters.com>
2013-02-19 15:34:00 +0000

    home reviewed

--- js/coarco.js
+++ js/coarco.js
@@ -5,6 +5,13 @@ jQuery(document).ready(function($){
         jQuery(this).append("<div class='block_left_expand'></div>");
         jQuery(this).append("<div class='block_right_expand'></div>");
     });
+    jQuery(".view-slideshow-portada .views-field-field-subtitulo-slideshow").each( function(){
+        jQuery(this).append("<div class='block_right_wing'></div>");
+    });
+    jQuery(".view-slideshow-portada .views-field-title").each( function(){
+        jQuery(this).append("<div class='block_left_wing'></div>");
+    });
+    
     jQuery("#om-menu-productos .om-maximenu-content").hover(
         function(){
            jQuery("#om-menu-productos .om-link").addClass("over"); 
