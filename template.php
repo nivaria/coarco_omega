b3a4c91a18f66dbde8a3358f0ba03ce9add45c83
Roberto Munoz <rmunglez@gmail.com>
2013-02-22 12:55:08 +0000

    Añadir página actual a la breadcrumb

--- template.php
+++ template.php
@@ -28,6 +28,7 @@ function coarco_omega_breadcrumb($variables) {
   $breadcrumb = $variables['breadcrumb'];
 
   if (!empty($breadcrumb)) {
+    $breadcrumb[] = drupal_get_title();
     // Provide a navigational heading to give context for breadcrumb links to
     // screen-reader users. Make the heading invisible with .element-invisible.
     $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
