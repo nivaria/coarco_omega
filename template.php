3c7a26f85c2cd6eba966f171d5313b34d05fbd1a
rmunglez <rmglez@anaga-mobile>
2013-02-27 11:35:18 +0000

    Modificada la generación de breadcrumbs

--- template.php
+++ template.php
@@ -15,14 +15,23 @@ function coarco_omega_preprocess_page(&$vars) {
   if (isset($_GET['overlay']) && $_GET['overlay'] == 'true') {
     $vars['theme_hook_suggestions'][] = 'page__overlay';
   }
+  $breadcrumb = drupal_get_breadcrumb(); 
   if (isset($vars['node']) && $vars['node']->type == "empresa") {
     $title = isset($vars['node']->field_nombre_comercial_empresa[LANGUAGE_NONE]) ?
       $vars['node']->field_nombre_comercial_empresa[LANGUAGE_NONE][0]['value']
       :
       $vars['node']->title;
-    drupal_set_title($title);
-    $vars['title'] = $title;
-  }  
+    if (count($breadcrumb) < 2) {
+      drupal_set_title($title);
+      $vars['title'] = $title;
+      $breadcrumb[] = $title;
+      drupal_set_breadcrumb($breadcrumb);
+    }
+  }
+  else {
+    $breadcrumb[] = drupal_get_title();
+    drupal_set_breadcrumb($breadcrumb);
+  }
 }
 
 function coarco_omega_preprocess_html(&$vars) {
@@ -30,13 +39,12 @@ function coarco_omega_preprocess_html(&$vars) {
     $vars['theme_hook_suggestions'][] = 'html__overlay';
   }
   
-  drupal_add_css("http://fonts.googleapis.com/css?family=Exo","external");
+  drupal_add_css("http://fonts.googleapis.com/css?family=Exo", "external");
 }
+
 function coarco_omega_breadcrumb($variables) {
   $breadcrumb = $variables['breadcrumb'];
-
   if (!empty($breadcrumb)) {
-    $breadcrumb[] = drupal_get_title();
     // Provide a navigational heading to give context for breadcrumb links to
     // screen-reader users. Make the heading invisible with .element-invisible.
     $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
